2c319f9000d92a667122873f2a1d2fb41d32badb
calebetadeumatosdasilva <calebetadeu@gmail.com>
2024-05-19 18:58:27 -0300

    first commit

---
+++ app/src/main/java/com/example/routine/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.routine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.routine.ui.theme.RoutineTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RoutineTheme {
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text =  "Routine App")
+                    Button(onClick = {  }) {
+                        Text(text = "Add Routine")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/routine/RoutineApplication.kt
@@ -0,0 +1,7 @@
+package com.example.routine
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RoutineApplication : Application()
